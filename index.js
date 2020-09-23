acbb70b4f0a4cce05c66fa15a7ee0545ec8ee917
koriwan <55373668+koriwan@users.noreply.github.com>
2020-09-23 16:45:18 +0900

    online/offline event
    인터넷 접속/비접속 시의 상태를 인식함!

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const title = document.querySelector("#title");
 
 const BASE_COLOR = "rgb(52, 73, 94)";
 const OTHER_COLOR = "rgb(142, 68, 173)"
@@ -13,7 +14,19 @@ function handleClick(){
 
 function init(){
     title.style.color = BASE_COLOR;
-    title.addEventListener("click", handleClick);    
+    title.addEventListener("mouseenter", handleClick);    
 }
 
 init();
+
+function handleOnline() {
+    console.log("you are using Internet!");
+}
+
+function handleOffline() {
+    console.log("you are out of internet!");
+}
+
+
+window.addEventListener("online", handleOnline);
+window.addEventListener("offline", handleOffline);
